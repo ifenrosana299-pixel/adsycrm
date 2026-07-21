59a5897c6b60e1060a90a345db838174999bd664
ifenrosana299-pixel <ifenrosana299@gmail.com>
2026-07-21 14:22:31 +0700

    Add files via upload

--- js/shared.js
+++ js/shared.js
@@ -130,7 +130,7 @@ function escapeHtml(str) {
 async function loadSkuList() {
     if (skuList.length) return;
     try {
-        const { data } = await sbData.from('sku_produk').select('kode,nama_produk,durasi_hari,tier_silver_min,tier_gold_min,tier_platinum_min,tier_diamond_min');
+        const { data } = await sbData.from('sku_produk').select('kode,nama_produk,durasi_hari,tier_silver_min,tier_gold_min,tier_platinum_min,tier_diamond_min,point_per_qty');
         skuList = data || [];
     } catch(_) { skuList = []; }
 }
